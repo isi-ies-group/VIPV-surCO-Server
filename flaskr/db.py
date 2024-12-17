94b36ca608cef938d753936f13b7038da73e1758
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-12-17 11:03:32 +0100

    delete unused files

--- flaskr/db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-engine = create_engine("sqlite:///instance/Photovolta.sqlite")
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
