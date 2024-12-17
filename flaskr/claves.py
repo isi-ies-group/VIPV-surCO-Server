1449cae786c2d794cd6577f96fb4719cee3d161e
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-12-17 09:28:07 +0100

    compat with Py3.10

--- flaskr/claves.py
+++ flaskr/claves.py
@@ -1,18 +1,24 @@
-import tomllib
+try:
+    import tomllib
+except ImportError:
+    import toml as tomllib
 import os
 
 
 SECRETS_PATH = os.path.abspath(os.path.join(__file__, "../../secrets/secrets.toml"))
 
 if os.path.exists(SECRETS_PATH):
-    with open(SECRETS_PATH, "rb") as f:
-        secrets = tomllib.load(f)
+    try:
+        with open(SECRETS_PATH, "rb") as f:
+            secrets = tomllib.load(f)
+    except TypeError:
+        secrets = tomllib.load(SECRETS_PATH)
     claveTokens = secrets["claveTokens"]
     admin_pass = secrets["admin_pass"]
     GOOGLE_ID = secrets["GOOGLE_ID"]
     GOOGLE_SECRET = secrets["GOOGLE_SECRET"]
 else:
     claveTokens = os.environ.get("claveTokens")
-    admin_pass =  os.environ.get("admin_pass")
+    admin_pass = os.environ.get("admin_pass")
     GOOGLE_ID = os.environ.get("GOOGLE_ID")
     GOOGLE_SECRET = os.environ.get("GOOGLE_SECRET")
